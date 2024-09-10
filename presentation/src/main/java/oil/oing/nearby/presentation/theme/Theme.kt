b719c4fefc48ad6008f145b5ac40521e9f7fd819
inseonyun <yuninseon@naver.com>
2024-09-10 23:51:58 +0900

    feat: Set Status bar Color

--- presentation/src/main/java/oil/oing/nearby/presentation/theme/Theme.kt
+++ presentation/src/main/java/oil/oing/nearby/presentation/theme/Theme.kt
@@ -2,15 +2,19 @@
 
 package oil.oing.nearby.presentation.theme
 
+import android.app.Activity
 import androidx.compose.foundation.background
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.SideEffect
 import androidx.compose.runtime.compositionLocalOf
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.toArgb
+import androidx.compose.ui.platform.LocalView
 import androidx.compose.ui.semantics.semantics
 import androidx.compose.ui.semantics.testTagsAsResourceId
 
@@ -30,6 +34,14 @@ fun OilOingTheme(
         LocalColors provides colors,
         LocalBackgroundTheme provides background,
     ) {
+        val view = LocalView.current
+        if (!view.isInEditMode) {
+            SideEffect {
+                val window = (view.context as Activity).window
+                window.statusBarColor = colors.primary.toArgb()
+            }
+        }
+
         Box(
             modifier = Modifier
                 .background(background.color)
